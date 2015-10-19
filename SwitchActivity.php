28b37b4ea524c7f5fff004b584bd30f2aba05aca
1304963 <MatthewCKemp>
2015-10-19 15:54:43 +0100

    Test for Azure.

---
+++ SwitchActivity.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        </head>
+        <body>
+            <p>
+                <?php
+                    $myage = 20
+                ?>
+            </p>
+        </body>
+</html>
