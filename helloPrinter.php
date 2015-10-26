97fafffc9c8d7fd621a94c1cc87156edc7f22f69
1304963 <MatthewCKemp>
2015-10-26 14:55:12 +0000

    Attempt 2

--- helloPrinter.php
+++ helloPrinter.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <a href="http://example.com/search.html?query=cat&type=image">Blarg</a>
 </head>
 <body>
 <p>
     <?php
-       $query = $_GET["query"];
+        $query = $_GET["query"];
         $type = $_GET ["type"];
         echo "The query is " . $query . " and the type is " . $type;
     ?>
