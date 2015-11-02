2dc660360946ea869cc511a867419615e0465ab1
1304963 <MatthewCKemp>
2015-11-02 14:00:43 +0000

    Cookie Labwork

--- Homepage.php
+++ Homepage.php
@@ -5,7 +5,15 @@
 <body>
 <p>
     <?php
-
+        function displayAccessLevelInformation($accessLevel) {
+            if ($accessLevel == "standarduser"){
+                echo "<p>You are currently logged in as a standard user<p/>";
+            }
+            elseif ($accessLevel == "root") {
+                echo "<p>You are currently logged in as a root user</p>";
+                echo "<p>You now have access to additional administrative features</p>";
+            }
+        }
     ?>
 </p>
 </body>
